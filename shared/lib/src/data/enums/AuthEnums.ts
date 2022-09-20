545b983e40e8dbfb1549e4051a9c96016dea041c
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-09-20 18:06:20 -0300

    feat: adding enums related to the Auth process

---
+++ shared/lib/src/data/enums/AuthEnums.ts
@@ -0,0 +1,14 @@
+/**
+ * @category Enum
+ * @module AuthEnums
+ */
+
+/**
+ * Enumerated values for Auth types.
+ */
+export enum AuthTypeEnum {
+  LOGIN = 'login',
+  SSO = 'sso',
+  GOOGLE = 'google',
+  FACEBOOK = 'facebook',
+}
