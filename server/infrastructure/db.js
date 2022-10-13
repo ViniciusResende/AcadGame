a98c26177b1c78cf1c2656211ceeb3081eb3af21
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-13 00:23:53 -0300

    Feat: Configuring the database connection.

--- server/infrastructure/db.js
+++ server/infrastructure/db.js
@@ -1,3 +1,16 @@
-require('dotenv').config(); // Access to every environment variable in the system
+require('dotenv').config();
+const Sequelize = require('sequelize');
 
-// import Sequelize from 'sequelize';
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        timezone: '-03:00',
+    },
+);
+
+module.exports = sequelize;
