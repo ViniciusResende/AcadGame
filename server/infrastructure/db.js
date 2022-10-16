b7646f859fc5e5016721fd8af228d3310fa4ad96
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-16 20:21:13 -0300

    Feat: Checking if the db has authenticated correctly.

--- server/infrastructure/db.js
+++ server/infrastructure/db.js
@@ -13,4 +13,15 @@ const sequelize = new Sequelize(
     },
 );
 
+const checkConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully!');
+    } catch (error) {
+        console.error('Unable to connect to the database due to: \n', error);
+    }
+};
+
+checkConnection();
+
 module.exports = sequelize;
