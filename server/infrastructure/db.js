decf6e7ac063c0a4be3e7eca21fa74e3212103fa
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-16 20:20:25 -0300

    Fix: Fixing the dotenv library import.

--- server/infrastructure/db.js
+++ server/infrastructure/db.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+require('dotenv').config({path:__dirname+'/.env'})
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PASSWORD || "",
     {
-        port: process.env.DB_PORT,
         host: process.env.DB_HOST,
+        port: process.env.PORT,
         dialect: process.env.DB_DIALECT,
         timezone: '-03:00',
     },
