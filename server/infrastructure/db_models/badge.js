d5c7f3251d22e559ed4a33f942827d67dba18e2c
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-16 20:22:03 -0300

    Feat: Defining the badge database model.

---
+++ server/infrastructure/db_models/badge.js
@@ -0,0 +1,27 @@
+const database = require('../db');
+const Sequelize = require('sequelize');
+
+const Badge = database.define('Badges', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+    },
+    name: {
+        type: Sequelize.STRING,
+        allowNull: true
+    },
+    icon: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+});
+
+Badge.sync({alter: false, force: false})
+    .then(() => {
+        console.log('Badge table was (re)created');
+    })
+    .catch((err) => console.log(err));
+
+module.exports = Badge;
