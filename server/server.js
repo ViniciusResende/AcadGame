89231523b4392ecbf897bb0ba0a1e2df43b1962e
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-16 20:22:23 -0300

    Feat: Initializing a database and express connection.

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const Express = require('express');
+const server = Express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+server.use(Express.json());
+server.use(bodyParser.urlencoded({extended:true}));
+server.use(bodyParser.json());
+server.use(cookieParser());
+
+// IIF (Immediately Invoked Function) to test the database and "Contracts" table creation.
+(async() => {
+    const BADGE = require('./infrastructure/db_models/badge');
+})();
+
+server.listen(process.env.EXPRESS_PORT, () => {
+    console.log(`Express initialized at ${process.env.EXPRESS_PORT}`);
+});
