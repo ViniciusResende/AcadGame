a3d3b6aa3ad23cdfac31c719ff1233004a784efd
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-17 13:13:43 -0300

    Feat: Handling the database connection results.

--- server/domains/badge/queryBadgeDomain.js
+++ server/domains/badge/queryBadgeDomain.js
@@ -1,5 +1,37 @@
-require('./badgeDomain')
+const queryBadges = require('../../gates/badge/queryBadgeExitGate');
 
-class QueryBadge {
+class QueryBadgeDomain {
+    
+    async queryAllBadges() {
+        let queriedBadges = [];
+        
+        try {
+            const badges = await queryBadges.getAllBadges();
+            
+            for (const badge of badges) {
+                queriedBadges.push(badge);
+            }
+            
+            return queriedBadges;
+        }
+        catch (err) {
+            return err;
+        }
+    }
+    
+    async queryOneBadge(id) {
+        let queriedBadges = [];
 
+        try {
+            const BADGE = await queryBadges.getOneBadge(id);
+
+            queriedBadges.push(BADGE);
+            
+            return queriedBadges;
+        } catch (err) {
+            return err;
+        }
+    }
 }
+
+module.exports = new QueryBadgeDomain;
