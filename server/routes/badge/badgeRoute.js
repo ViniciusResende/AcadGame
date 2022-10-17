bb8788477cbf5bf429ee5b9e5f3634412d3827f7
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-17 13:15:01 -0300

    Feat: Defining two routes to get badges from the database.

--- server/routes/badge/badgeRoute.js
+++ server/routes/badge/badgeRoute.js
@@ -1,1 +1,27 @@
 const Router = require('express').Router();
+
+const QueryBadge = require('../../gates/badge/queryBadgeEntryGate');
+
+Router.get('/', async (req, res) => {
+    try {
+        const BADGES = await QueryBadge.searchAllBadges();
+
+        res.status(200).json(BADGES);
+    }
+    catch(err) {
+        res.status(500).send(err);
+    }
+});
+
+Router.get('/:id', async (req, res) => {
+    try {
+        const BADGE = await QueryBadge.getOneBadge(req.params.id);
+
+        res.status(200).json(BADGE);
+    }
+    catch(err) {
+        res.status(500).send(err);
+    }
+});
+
+module.exports = Router;
