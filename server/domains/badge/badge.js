562e0bb0f536635547b72500a34cc97a81201924
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-16 22:28:11 -0300

    Feat: Defining contructors and other methods.

--- server/domains/badge/badge.js
+++ server/domains/badge/badge.js
@@ -2,4 +2,29 @@ class Badge {
     id;
     name;
     url;
+
+    Badge() {
+        this.id = -1;
+        this.name = "";
+        this.url = "";
+    }
+
+    Badge(id, name, url) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getUrl() {
+        return this.url;
+    }
+
 }
