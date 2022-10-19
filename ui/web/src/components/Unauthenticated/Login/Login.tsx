08a9dcd38f4f07bd6003eec92a0b6329d80cc680
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-19 08:37:33 -0300

    feat: implementing submit logic to login form

--- ui/web/src/components/Unauthenticated/Login/Login.tsx
+++ ui/web/src/components/Unauthenticated/Login/Login.tsx
@@ -15,45 +15,60 @@ import { LogInIcon } from '../../../assets/svg/icons';
 import { HealthyStyleIllustration } from '../../../assets/svg/illustrations';
 import { LogoFull } from '../../../assets/svg/logo';
 
-function LoginIllustration() {
-  return (
-    <div className="login-page__illustration-container">
-      <h1>Uma conquista a cada passo.</h1>
-      <h3>
-        Conecte-se, evolua, compartilhe e torne sua jornada mais divertida com a
-        gente
-      </h3>
-      <HealthyStyleIllustration />
-    </div>
-  );
-}
+type LoginComponentProps = {
+  loginAuth: (username: string, password: string) => Promise<void>;
+};
+function LoginComponent({ loginAuth }: LoginComponentProps) {
+  function LoginIllustration() {
+    return (
+      <div className="login-page__illustration-container">
+        <h1>Uma conquista a cada passo.</h1>
+        <h3>
+          Conecte-se, evolua, compartilhe e torne sua jornada mais divertida com
+          a gente
+        </h3>
+        <HealthyStyleIllustration />
+      </div>
+    );
+  }
 
-function LoginContent() {
-  return (
-    <section className="login-page__form-container">
-      <LogoFull className="login-page__logo" />
-      <Input
-        className="login-page__input"
-        controlId="login"
-        inputLabel="Login"
-      />
-      <Input
-        className="login-page__input"
-        controlId="password"
-        inputLabel="Senha"
-        type="password"
-      />
-      <Button modifier="default" icon={<LogInIcon />}>
-        Entrar
-      </Button>
-      <span className="login-page__sign-up">
-        Não tem conta? <Link to={'/signUp'}>Cadastre-se no AcadGame</Link>
-      </span>
-    </section>
-  );
-}
+  function onSubmitForm(event: React.FormEvent) {
+    event.preventDefault();
+    //@ts-ignore
+    const { username, password } = event.target;
 
-function LoginComponent() {
+    loginAuth(username.value, password.value);
+  }
+
+  function LoginContent() {
+    return (
+      <section className="login-page__form-container">
+        <LogoFull className="login-page__logo" />
+        <form onSubmit={onSubmitForm}>
+          <Input
+            className="login-page__input"
+            controlId="login"
+            inputLabel="Login"
+            name="username"
+          />
+          <Input
+            className="login-page__input"
+            controlId="password"
+            inputLabel="Senha"
+            type="password"
+            name="password"
+          />
+          <Button modifier="default" icon={<LogInIcon />} type="submit">
+            Entrar
+          </Button>
+        </form>
+        <span className="login-page__sign-up">
+          Não tem conta? <Link to={'/signUp'}>Cadastre-se no AcadGame</Link>
+        </span>
+      </section>
+    );
+  }
+
   return (
     <UnauthenticatedPage
       illustrationContent={LoginIllustration()}
