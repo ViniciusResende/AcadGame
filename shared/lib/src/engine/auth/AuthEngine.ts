3a48223e2d2da7b7fd950a4cb6d026426a9c6840
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-19 08:36:04 -0300

    feat: persisting auth token after login

--- shared/lib/src/engine/auth/AuthEngine.ts
+++ shared/lib/src/engine/auth/AuthEngine.ts
@@ -3,8 +3,15 @@
  * @module AuthEngine
  */
 
-/** Utilities */
-import { Utilities } from '../../utils/Utilities';
+/** Enums */
+import { AuthTypeEnum } from '../../data/enums/AuthEnums';
+import { LocalStorageKeysEnum } from '../../utils/classes/local-storage/LocalStorageEnums';
+
+/** Interfaces */
+import { IApiAcadAuthResponse } from '../../resource/api/acad/ApiAcadInterfaces';
+
+/** Classes */
+import { LocalStorage } from '../../utils/classes/local-storage/LocalStorage';
 
 /** Access */
 import { AuthAccess } from '../../access/auth/AuthAccess';
@@ -14,8 +21,33 @@ import { AuthAccess } from '../../access/auth/AuthAccess';
  */
 export class AuthEngine {
   #authAccess: AuthAccess;
+  #localStorage: LocalStorage;
 
   constructor() {
     this.#authAccess = new AuthAccess();
+    this.#localStorage = new LocalStorage();
+  }
+
+  #persistAuthToken(token: string) {
+    this.#localStorage.setLocalStorageItem(
+      LocalStorageKeysEnum.AUTH_TOKEN,
+      token
+    );
+  }
+
+  async authenticateUser(
+    authType: AuthTypeEnum,
+    authBody: unknown
+  ): Promise<IApiAcadAuthResponse | null> {
+    let authPayload: IApiAcadAuthResponse | null = null;
+    try {
+      authPayload = await this.#authAccess.auth(authType, authBody);
+      if (authPayload.token) {
+        this.#persistAuthToken(authPayload.token);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    return authPayload;
   }
 }
