ef88b0c175b98a56ad1989108df13bf3fd2ef3bd
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-20 16:58:42 -0300

    Feat: Defining the User database model.

---
+++ server/infrastructure/models/user.js
@@ -0,0 +1,35 @@
+const database = require('../db');
+const Sequelize = require('sequelize');
+
+const User = database.define('Users', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+    nickName: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    score: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+});
+
+User.sync({
+    alter: false,
+    force: false
+}).then( () => {
+    console.log('User table was (re)created');
+}).catch(err => {
+    console.log(err);
+});
+
+module.exports = User;
