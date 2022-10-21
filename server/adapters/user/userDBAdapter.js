9da144b4fb781dbc3eb2a5665cb2fff4d300ff48
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-21 01:47:21 -0300

    Feat: Creating adapter to get an user by nickname resemblance.

--- server/adapters/user/userDBAdapter.js
+++ server/adapters/user/userDBAdapter.js
@@ -1,5 +1,7 @@
 const USER = require('../../infrastructure/models/user');
 
+const { Op } = require('sequelize');
+
 class UserDatabaseAdapter {
     unnecessaryAttributes = ['createdAt', 'updatedAt', 'password'];
 
@@ -53,6 +55,29 @@ class UserDatabaseAdapter {
             }
 
             return QUERIED_USER;
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    async getUsersByNick(nickname) {
+        try {
+            const QUERIED_USERS = USER.findAll({
+                where: {
+                    nickName: {
+                        [Op.like]: `%${nickname}%`
+                    }
+                },
+                attributes: {
+                    exclude: this.unnecessaryAttributes
+                }
+            });
+
+            if (!QUERIED_USERS) {
+                throw new Error('Não encontramos um usuário com nome semelhante a esse.');
+            }
+            
+            return QUERIED_USERS;
         } catch (err) {
             throw err;
         }
