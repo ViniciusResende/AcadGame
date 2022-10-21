065c3a65b19b070a4db9f8ce1d82c5d0c99a2024
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-20 23:26:19 -0300

    feat: adding user get data type enums

---
+++ shared/lib/src/data/enums/UserEnums.ts
@@ -0,0 +1,13 @@
+/**
+ * @category Enum
+ * @module UserEnums
+ */
+
+/**
+ * Enumerated values for User get Data types.
+ */
+export enum UserGetDataTypeEnum {
+  USERINFO = 'userinfo',
+  HISTOGRAM = 'histogram',
+  BADGES = 'badges',
+}
