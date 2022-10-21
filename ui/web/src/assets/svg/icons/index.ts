cdfd4163b3a45d7035a7e467cef745d5d3ca99be
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-20 23:29:31 -0300

    feat: adding new person icon svg

--- ui/web/src/assets/svg/icons/index.ts
+++ ui/web/src/assets/svg/icons/index.ts
@@ -1,5 +1,6 @@
 import EyeIcon from './eye.svg';
 import EyeSlashIcon from './eye-slash.svg';
 import LogInIcon from './log-in.svg';
+import PersonIcon from './person.svg';
 
-export { EyeIcon, EyeSlashIcon, LogInIcon };
+export { EyeIcon, EyeSlashIcon, LogInIcon, PersonIcon };
