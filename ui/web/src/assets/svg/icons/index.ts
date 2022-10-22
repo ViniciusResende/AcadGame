cbcac83127b6cec75f52d8b062be7966fe961cf4
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-22 07:46:16 -0300

    feat: adding new Pencil and Trophy Star Icons

--- ui/web/src/assets/svg/icons/index.ts
+++ ui/web/src/assets/svg/icons/index.ts
@@ -1,6 +1,15 @@
 import EyeIcon from './eye.svg';
 import EyeSlashIcon from './eye-slash.svg';
 import LogInIcon from './log-in.svg';
+import PencilIcon from './pencil.svg';
 import PersonIcon from './person.svg';
+import TrophyStarIcon from './trophy-star.svg';
 
-export { EyeIcon, EyeSlashIcon, LogInIcon, PersonIcon };
+export {
+  EyeIcon,
+  EyeSlashIcon,
+  LogInIcon,
+  PencilIcon,
+  PersonIcon,
+  TrophyStarIcon,
+};
