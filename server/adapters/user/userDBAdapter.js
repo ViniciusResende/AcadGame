000b6ada7d810707cbece384db42aafd82cf359f
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-22 01:15:02 -0300

    Feat: Defining a method to create a new user in the database.

--- server/adapters/user/userDBAdapter.js
+++ server/adapters/user/userDBAdapter.js
@@ -1,10 +1,50 @@
 const USER = require('../../infrastructure/models/user');
 
+const bcrypt = require('bcrypt');
+
 const { Op } = require('sequelize');
 
 class UserDatabaseAdapter {
     unnecessaryAttributes = ['createdAt', 'updatedAt', 'password'];
 
+    USER_REQUIRED_INFO = ['nickName', 'email', 'password'];
+
+    async newUser(userInfo) {
+        let user = new Object;
+
+        try {
+            // Assuring the userInfo object only contains valid properties
+            Object.keys(userInfo).forEach(info => {
+                if (this.USER_REQUIRED_INFO.includes(info)) {
+                    user[info] = userInfo[info];
+                }
+            });
+
+            // Assuring there are no necessary information being left behind
+            for (const INFO of this.USER_REQUIRED_INFO) {
+                if (!user[INFO]) {
+                    throw new Error(`A informação ${INFO} é necessária para concluir o cadastro.`);
+                }
+            }
+            
+            const saltRounds = 10;
+            user.password = await bcrypt.hash(user.password, saltRounds);
+
+            if(await USER.findOne({
+                where: {
+                    email: userInfo.email
+                } 
+            }) != null) {
+                throw new Error("Usuário já cadastrado.");
+            }
+
+            await USER.create(user);
+        } 
+        catch (err) {
+            throw err;
+        }
+    }
+    
     async getEveryUser() {
         try {
             const ALL_USERS = await USER.findAll({
