8c4a3a0baccea811de2882194ec502696c1fbe1a
Carlos Leão <caikleao@gmail.com>
2022-10-22 12:36:00 -0300

    Feat: Creating the Exercise domain file.

---
+++ server/domains/exercise/exerciseDomain.js
@@ -0,0 +1,35 @@
+const queryExercises = require('../../gates/exercise/exerciseExitGate');
+
+class QueryExerciseDomain {
+    async queryAllExercises() {
+        let queriedExercises = [];
+
+        try {
+            const exercises = await queryExercises.getAllExercises();
+
+            for (const exercise of exercises) {
+                queriedExercises.push(exercise);
+            }
+
+            return queriedExercises;
+        }
+        catch(err) {
+            return err;
+        }
+    }
+
+    async queryOneExercise(id) {
+        let queriedExercises = [];
+
+        try {
+            const EXERCISE = await queryExercises.getOneExercise(id);
+
+            queriedExercises.push(EXERCISE);
+
+            return queriedExercises;
+        }
+        catch (err) {
+            return err;
+        }
+    }
+}
