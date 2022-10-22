0f0dbef6a7f916c9e54443686692f25a1d56b820
Carlos Leão <caikleao@gmail.com>
2022-10-22 12:36:43 -0300

    Feat: Defining the Exercise route.

---
+++ server/adapters/exercise/exerciseRoute.js
@@ -0,0 +1,27 @@
+const Router = require('express').Router();
+
+const QueryExercise = require('../../gates/exercise/exerciseEntryGate');
+
+Router.get('/', async (req, res) => {
+    try {
+        const EXERCISES = await QueryExercise.searchAllExercises();
+
+        res.status(200).json(EXERCISES);
+    }
+    catch(err) {
+        res.status(500).send(err);
+    }
+});
+
+Router.get('/:id', async (req, res) => {
+    try {
+        const EXERCISE = await QueryExercise.getOneExercise();
+
+        res.status(200).json(EXERCISE);
+    }
+    catch(err) {
+        res.status(500).send(err);
+    }
+});
+
+module.exports = Router;
