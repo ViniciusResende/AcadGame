090dcb16b8ce646c4c62e0e4c9401e1cf6f86936
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-22 19:29:48 -0300

    feat: added new icons

--- ui/web/src/assets/svg/icons/index.ts
+++ ui/web/src/assets/svg/icons/index.ts
@@ -1,14 +1,18 @@
+import BullseyeArrowIcon from './bullseye-arrow.svg';
 import EyeIcon from './eye.svg';
 import EyeSlashIcon from './eye-slash.svg';
 import LogInIcon from './log-in.svg';
+import MetricsChartIcon from './metrics-chart.svg';
 import PencilIcon from './pencil.svg';
 import PersonIcon from './person.svg';
 import TrophyStarIcon from './trophy-star.svg';
 
 export {
+  BullseyeArrowIcon,
   EyeIcon,
   EyeSlashIcon,
   LogInIcon,
+  MetricsChartIcon,
   PencilIcon,
   PersonIcon,
   TrophyStarIcon,
