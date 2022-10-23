aded20d6e000e298924b6cdc0e986cf600583b93
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-23 18:39:24 -0300

    Feat: Creating methods to retrieve data from users table.

--- server/gates/user/userExitGate.js
+++ server/gates/user/userExitGate.js
@@ -28,6 +28,48 @@ class QueryUser {
             throw err;
         }
     }
+
+    async getUserByEmail(email) {
+        try {
+            const QUERIED_USER = await USER_DB_ADAPTER.getUserByEmail(email);
+
+            return QUERIED_USER;
+        } 
+        catch (err) {
+            throw err;
+        }
+    }
+
+    async getUserByNickName(nickname) {
+        try {
+            const QUERIED_USER = await USER_DB_ADAPTER.getUsersByNick(nickname);
+
+            return QUERIED_USER;
+        } 
+        catch (err) {
+            throw err;
+        }
+    }
+
+    async getTopRankUsers(rank) {
+        try {
+            let topRankUsers = [];
+
+            const TOP_USERS = await USER_DB_ADAPTER.getTopUsers();
+
+            let count = 0;
+            TOP_USERS.forEach(user => {
+                if (count < rank) topRankUsers.push(user.dataValues);
+
+                count++;
+            });
+
+            return topRankUsers
+        }
+        catch (err) {
+            
+        }
+    }
 }
 
 module.exports = QueryUser;
