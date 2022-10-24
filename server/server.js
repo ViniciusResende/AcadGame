a76662feda7261e5717aefdc61178539beb2c5ad
Carlos Leão <caikleao@gmail.com>
2022-10-24 18:10:06 -0300

    Feat: Adding User Route.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const server = Express();
 
 const BadgeRouter = require('./adapters/badge/badgeRoute');
 const ExerciseRouter = require('./adapters/exercise/exerciseRoute');
+const USER_ROUTER = require('./adapters/user/userRoute');
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -14,6 +15,7 @@ server.use(cookieParser());
 
 server.use('/badges', BadgeRouter);
 server.use('/exercises', ExerciseRouter);
+server.use('/users', USER_ROUTER);
 
 server.listen(process.env.EXPRESS_PORT, () => {
     console.log(`Express initialized at ${process.env.EXPRESS_PORT}`);
