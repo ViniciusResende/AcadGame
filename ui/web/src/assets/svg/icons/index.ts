cc546224b4c90185cfd6e5a3c872a15aee05afd9
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-25 09:56:19 -0300

    feat: adding new check and xmark icons

--- ui/web/src/assets/svg/icons/index.ts
+++ ui/web/src/assets/svg/icons/index.ts
@@ -1,4 +1,5 @@
 import BullseyeArrowIcon from './bullseye-arrow.svg';
+import CheckIcon from './check.svg';
 import EyeIcon from './eye.svg';
 import EyeSlashIcon from './eye-slash.svg';
 import LogInIcon from './log-in.svg';
@@ -6,9 +7,11 @@ import MetricsChartIcon from './metrics-chart.svg';
 import PencilIcon from './pencil.svg';
 import PersonIcon from './person.svg';
 import TrophyStarIcon from './trophy-star.svg';
+import XMarkIcon from './xmark.svg';
 
 export {
   BullseyeArrowIcon,
+  CheckIcon,
   EyeIcon,
   EyeSlashIcon,
   LogInIcon,
@@ -16,4 +19,5 @@ export {
   PencilIcon,
   PersonIcon,
   TrophyStarIcon,
+  XMarkIcon,
 };
