78d0a87a913bab2518375e73b1b803666ea7c769
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-26 07:43:59 -0300

    feat: adding new profile picture images options

--- ui/web/src/assets/images/profilePics/index.ts
+++ ui/web/src/assets/images/profilePics/index.ts
@@ -1,5 +1,69 @@
 import DefaultProfilePic from './default.png';
 import GenericManOne from './generic-man-one.png';
+import GenericManTwo from './generic-man-two.png';
+import GenericManThree from './generic-man-three.png';
+import GenericManFour from './generic-man-four.png';
+import GenericManFive from './generic-man-five.png';
+import GenericWomanOne from './generic-woman-one.png';
+import GenericWomanTwo from './generic-woman-two.png';
+import GenericWomanThree from './generic-woman-three.png';
+import GenericWomanFour from './generic-woman-four.png';
+import GenericWomanFive from './generic-woman-five.png';
+import SportAmericanFootball from './sport-americanfootbal.png';
+import SportBasketball from './sport-basketball.png';
+import SportBicycle from './sport-bicycle.png';
+import SportFight from './sport-fight.png';
+import SportFootball from './sport-football.png';
+import SportHandball from './sport-handball.png';
+import SportRun from './sport-run.png';
+import SportSurf from './sport-surf.png';
+import SportSwimming from './sport-swimming.png';
+import SportTennis from './sport-tennis.png';
+import FunnyIHateLegDay from './funny-ihatelegday.png';
+import FunnyBodyBuilder from './funny-bodybuilder.png';
+import SpecialAssassinsScreed from './special-assasinsscreed.png';
 import SpecialBuzz from './special-buzz.png';
+import SpecialCS from './special-cs.png';
+import SpecialGodOfWar from './special-godofwar.png';
+import SpecialGoku from './special-goku.png';
+import SpecialGTA from './special-gta.png';
+import SpecialMario from './special-mario.png';
+import SpecialSpike from './special-spike.png';
+import SpecialTheSims from './special-thesims.png';
+import SpecialZelda from './special-zelda.png';
 
-export { DefaultProfilePic, GenericManOne, SpecialBuzz };
+export {
+  DefaultProfilePic,
+  GenericManOne,
+  GenericManTwo,
+  GenericManThree,
+  GenericManFour,
+  GenericManFive,
+  GenericWomanOne,
+  GenericWomanTwo,
+  GenericWomanThree,
+  GenericWomanFour,
+  GenericWomanFive,
+  SportAmericanFootball,
+  SportBasketball,
+  SportBicycle,
+  SportFight,
+  SportFootball,
+  SportHandball,
+  SportRun,
+  SportSurf,
+  SportSwimming,
+  SportTennis,
+  FunnyIHateLegDay,
+  FunnyBodyBuilder,
+  SpecialAssassinsScreed,
+  SpecialBuzz,
+  SpecialCS,
+  SpecialGodOfWar,
+  SpecialGoku,
+  SpecialGTA,
+  SpecialMario,
+  SpecialSpike,
+  SpecialTheSims,
+  SpecialZelda,
+};
