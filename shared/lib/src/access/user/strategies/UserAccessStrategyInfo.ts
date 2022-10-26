ea1abfdf56ddcee4a93d2b4ea21ee7dcfd6f86d4
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-25 23:06:52 -0300

    feat: implementing update data method and data caching to info strategy

--- shared/lib/src/access/user/strategies/UserAccessStrategyInfo.ts
+++ shared/lib/src/access/user/strategies/UserAccessStrategyInfo.ts
@@ -4,7 +4,10 @@
  */
 
 /** Interfaces */
-import { IApiUserGetDataInfoResponse } from '../../../resource/api/acad/ApiAcadInterfaces';
+import {
+  IApiUserGetDataInfoResponse,
+  IApiAcadUpdateUserInfoBody,
+} from '../../../resource/api/acad/ApiAcadInterfaces';
 
 /** Classes */
 import { UserAccessStrategy } from './UserAccessStrategy';
@@ -13,9 +16,35 @@ import { UserAccessStrategy } from './UserAccessStrategy';
  * Class that specifies User Get Data access strategy
  */
 export class UserAccessStrategyInfo extends UserAccessStrategy {
+  #userInfoCache: IApiUserGetDataInfoResponse | null;
+
+  constructor() {
+    super();
+    this.#userInfoCache = null;
+  }
+
   async getData(token: string): Promise<IApiUserGetDataInfoResponse> {
+    if (this.#userInfoCache) return this.#userInfoCache;
+
     const getUserInfoResponse = await this.api.userInfo(token);
 
+    this.#userInfoCache = getUserInfoResponse;
     return getUserInfoResponse;
+  }
+
+  async updateData(
+    token: string,
+    updateDataBody: IApiAcadUpdateUserInfoBody
+  ): Promise<IApiUserGetDataInfoResponse> {
+    const { nickname, picture } = updateDataBody;
+    const updatedUserInfoResponse = await this.api.updateUserInfo(
+      token,
+      nickname,
+      picture
+    );
+
+    this.#userInfoCache = updatedUserInfoResponse;
+
+    return updatedUserInfoResponse;
   }
 }
