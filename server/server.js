ae818d849045867d4d9979e8aba66e5cf0e94ee0
Carlos Leão <caikleao@gmail.com>
2022-10-27 15:51:36 -0300

    Feat: Adding /exercise-sheets route to server.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const server = Express();
 const BADGE_ROUTER = require('./adapters/badge/badgeRoute');
 const EXERCISE_ROUTER = require('./adapters/exercise/exerciseRoute');
 const USER_ROUTER = require('./adapters/user/userRoute');
+const EXERCISE_SHEET_ROUTER = require('./adapters/exerciseSheet/exerciseSheetRouter');
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -16,6 +17,7 @@ server.use(cookieParser());
 server.use('/badges', BADGE_ROUTER);
 server.use('/exercises', EXERCISE_ROUTER);
 server.use('/users', USER_ROUTER);
+server.use('/exercise-sheets', EXERCISE_SHEET_ROUTER);
 
 server.listen(process.env.EXPRESS_PORT, () => {
     console.log(`Express initialized at ${process.env.EXPRESS_PORT}`);
