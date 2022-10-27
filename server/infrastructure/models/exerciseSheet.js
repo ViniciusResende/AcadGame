49b28b8632c193601a83ebc595a165f2e073f345
Carlos Leão <caikleao@gmail.com>
2022-10-27 15:47:33 -0300

    Feat: Creating the ExerciseSheet database model.

---
+++ server/infrastructure/models/exerciseSheet.js
@@ -0,0 +1,46 @@
+const database = require('../db');
+const Sequelize = require('sequelize');
+const User = require('./user');
+const Exercise = require('./exercise');
+
+const ExerciseSheet = database.define('ExerciseSheets', {
+    numSheet: {
+        type:Sequelize.INTEGER,
+        allowNull: false
+    },
+    userId: {
+        type:Sequelize.INTEGER,
+        allowNull: false
+    },
+    exerciseId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    load: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+    },
+    time: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+    },
+    numRepetitions: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+    },
+    numSets: {
+        type: Sequelize.INTEGER,
+        allowNull: true
+    }
+}); 
+
+// User.belongsToMany(Exercise, { through: ExerciseSheet });
+// Exercise.belongsToMany(User, { through: ExerciseSheet });
+
+ExerciseSheet.sync({alter: false, force: false})
+    .then(() => {
+        console.log('ExerciseSheet table was (re)created');
+    })
+    .catch((err) => console.log(err));
+
+module.exports = ExerciseSheet;
