11d0e0a520fe605c767d2abe300153caec023e72
Carlos Leão <caikleao@gmail.com>
2022-10-27 15:48:36 -0300

    Feat: Creating ExerciseSheet database adapter.

---
+++ server/adapters/exerciseSheet/exerciseSheetDBAdapter.js
@@ -0,0 +1,29 @@
+const EXERCISE_SHEET = require('../../infrastructure/models/exerciseSheet');
+
+class ExerciseSheetDatabaseAdapter {
+    unnecessaryAttributes = ['createdAt', 'updatedAt'];
+
+    async findUserExerciseSheets(userId) {
+        try {
+            const QUERIED_USER_EXERCISES = await EXERCISE_SHEET.findAll({
+                where: {
+                    UserId: userId
+                },
+                attributes: {
+                    exclude: this.unnecessaryAttributes
+                }
+            });
+
+            if(!QUERIED_USER_EXERCISES) {
+                throw new Error(`Não encontramos fichas de exercícios para o usuário com id ${userId}`);
+            }
+
+            return QUERIED_USER_EXERCISES;
+        }
+        catch(err) {
+            throw err;
+        }
+    }
+}
+
+module.exports = new ExerciseSheetDatabaseAdapter;
