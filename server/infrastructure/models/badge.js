d39205e783284d71294aa6f3dd1245707384914e
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-27 02:05:58 -0300

    Feat: Adding the unlockScore attribute to the badge table.

--- server/infrastructure/models/badge.js
+++ server/infrastructure/models/badge.js
@@ -15,13 +15,20 @@ const Badge = database.define('Badges', {
     icon: {
         type: Sequelize.STRING,
         allowNull: false,
+    },
+    unlockScore: {
+        type: Sequelize.INTEGER,
+        allowNull: false
     }
 });
 
-Badge.sync({alter: false, force: false})
-    .then(() => {
-        console.log('Badge table was (re)created');
-    })
-    .catch((err) => console.log(err));
+Badge.sync({
+    alter: true,
+    force: true
+}).then(() => {
+    console.log('Badge table was (re)created');
+}).catch((err) => {
+    console.log(err)
+});
 
 module.exports = Badge;
