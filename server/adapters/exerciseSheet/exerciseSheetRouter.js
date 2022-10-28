b344409a9276b544dcfed3533ffedfd0a34b440d
Carlos Leão <caikleao@gmail.com>
2022-10-27 21:24:55 -0300

    Ref: Changing variable name.

--- server/adapters/exerciseSheet/exerciseSheetRouter.js
+++ server/adapters/exerciseSheet/exerciseSheetRouter.js
@@ -3,12 +3,12 @@ const Router = require('express').Router();
 const QueryExerciseSheet = require('../../domains/exerciseSheet/exerciseSheetDomain');
 
 Router.get('/', async (req, res) => {
-    let userId = req.query.userID;
+    let userId = req.query.userID; // TODO: pegar do JWT
 
     try {
-        const EXERCISE_SHEET = await QueryExerciseSheet.queryUserExerciseSheets(userId);
+        const EXERCISE_SHEETS = await QueryExerciseSheet.queryUserExerciseSheets(userId);
 
-        res.status(200).json(EXERCISE_SHEET);
+        res.status(200).json(EXERCISE_SHEETS);
     }
     catch(err) {
         res.status(500).send(err);
