8d76e9da4f9173a3603be8c10fc54e117bb66131
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-28 00:23:15 -0300

    Feat: Defining authentication domain primary structure.

---
+++ server/domains/authentication/authDomain.js
@@ -0,0 +1,14 @@
+const USER_SERVICES = require('../../gates/user/userExitGate');
+
+class Auth {
+    async login(email, password) {
+        try {
+            
+        }
+        catch (err) {
+            
+        }
+    }
+}
+
+module.exports = new Auth;
