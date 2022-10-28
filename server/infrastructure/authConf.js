ef3b72d7f9d76306201c18875dbd9f7a8cc96d03
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-28 01:48:48 -0300

    Feat: Defining the serializing an deserializing methods.

--- server/infrastructure/authConf.js
+++ server/infrastructure/authConf.js
@@ -1,10 +1,22 @@
-const USER_DB_ADAPTER = require('../user/userDBAdapter');
+const USER = require('../domains/user/userDomain');
 
 const BCRYPT = require('bcrypt');
 
 const LOCAL_STRATEGY = require('passport-local').Strategy;
 
 
-module.exports = (passport) => {
-    
+module.exports = async (passport) => {
+    passport.serializeUser( (user, done) => {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser( (id, done) => {
+        try {
+            const CURRENT_USER = USER.getSingleUser(id);
+            done(null, CURRENT_USER);
+        }
+        catch (err) {
+            done(err, null);
+        }
+    });
 };
