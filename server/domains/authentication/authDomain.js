af2b2fe14c5705908b9faba5706a3677f48315e6
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-28 00:31:43 -0300

    Feat: Verifying if the user trying to login is currently enrolled in the platform.

--- server/domains/authentication/authDomain.js
+++ server/domains/authentication/authDomain.js
@@ -3,10 +3,13 @@ const USER_SERVICES = require('../../gates/user/userExitGate');
 class Auth {
     async login(email, password) {
         try {
-            
+            const USER = USER_SERVICES.getUserByEmail(email);
+            if (!USER) {
+                throw new Error('Usuário não cadastrado.');
+            }
         }
         catch (err) {
-            
+            throw err;
         }
     }
 }
