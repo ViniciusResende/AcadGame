371e09ebaf87c2a066f190af590af281251692cd
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-28 01:44:13 -0300

    Feat: Initializing the passport authentication file.

---
+++ server/infrastructure/authConf.js
@@ -0,0 +1,10 @@
+const USER_DB_ADAPTER = require('../user/userDBAdapter');
+
+const BCRYPT = require('bcrypt');
+
+const LOCAL_STRATEGY = require('passport-local').Strategy;
+
+
+module.exports = (passport) => {
+    
+};
