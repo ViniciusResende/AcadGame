d2aac3ead2058ac27be74d6abdf17f9f78bcea05
Carlos Leão <caikleao@gmail.com>
2022-10-28 11:26:50 -0300

    Feat: Adding function to post a user exercise from a correct userExerciseInfo.

--- server/gates/exerciseSheet/exerciseSheetExitGate.js
+++ server/gates/exerciseSheet/exerciseSheetExitGate.js
@@ -1,7 +1,10 @@
 const ExerciseSheetDBAdapter = require('../../adapters/exerciseSheet/exerciseSheetDBAdapter');
-const Exercise = require('../../infrastructure/models/exercise');
 
 class QueryExerciseSheetDB {
+    EVERY_USER_EXERCISE_INFO = ['numSheet', 'userId', 'exerciseId', 'load', 'time', 'numRepetitions', 'numSets'];
+
+    USER_EXERCISE_REQUIRED_INFO = ['numSheet', 'userId', 'exerciseId'];
+
     async getUserExerciseSheets(userId) {
         try {
             const QUERIED_USER_EXERCISES = await ExerciseSheetDBAdapter.findUserExerciseSheets(userId);
@@ -16,6 +19,31 @@ class QueryExerciseSheetDB {
         }
         catch(err) {
             return err;
+        }
+    }
+
+    async postUserExercise(userExerciseInfo) {
+        let newUserExercise = new Object;
+
+        try {
+            // userInfo object must contains only valid properties
+            Object.keys(userExerciseInfo).forEach(info => {
+                if (this.EVERY_USER_EXERCISE_INFO.includes(info)) {
+                    newUserExercise[info] = userExerciseInfo[info];
+                }
+            });
+
+            // verify if no necessary information being left behind
+            for(const INFO of this.USER_EXERCISE_REQUIRED_INFO) {
+                if(!newUserExercise[INFO]) {
+                    throw new Error(`A informação ${INFO} é necessária para concluir o cadastro do exercício de usuário.`)
+                }
+            }
+    
+            await ExerciseSheetDBAdapter.newUserExercise(newUserExercise);
+        }
+        catch(err) {
+            throw err;
         }
     }
 }
