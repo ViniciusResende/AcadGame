b818890d4b7a6da2b0f1a483073a0f6a530a8fa0
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-28 00:18:43 -0300

    Feat: Defining response return statements.

--- server/adapters/authentication/loginRoute.js
+++ server/adapters/authentication/loginRoute.js
@@ -1,5 +1,18 @@
 const ROUTER = require('express').Router();
 
 ROUTER.post('/', async (req, res) => {
+    try {
+        const EMAIL = req.body.email;
+        const PASSWORD = req.body.password;
 
+        res.status(200).json({
+            message: "enjoy your token :)",
+            token: "123"
+        });
+    }
+    catch (err) {
+        res.status(500).send(err.message);
+    }
 });
+
+module.exports = ROUTER;
