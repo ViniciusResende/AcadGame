83840d2d25765bbbe6364d3da07dc06aa503f522
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-28 10:53:49 -0300

    feat: implementing useSecurity hook in the AuthenticatedLayout

--- ui/web/src/components/Layout/Authenticated/AutehnticatedLayout.tsx
+++ ui/web/src/components/Layout/Authenticated/AutehnticatedLayout.tsx
@@ -2,12 +2,18 @@
 import React, { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
+/** React Hooks */
+import useSecurity from '../../../routes/middlewares/useSecurity';
+
 /** React Component */
 import Header from './elements/Header';
+
 /** Styles */
 import './AuthenticatedLayout.scss';
 
 function AuthenticatedLayout() {
+  useSecurity();
+
   return (
     <Suspense fallback="loading">
       <div className="authenticated-page">
