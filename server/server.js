eb8e38afee67284682b75fc83d4c807815d1455c
Carlos Leão <caikleao@gmail.com>
2022-10-29 20:26:00 -0300

    Feat: Adding day score router to server.

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const server = Express();
 const BADGE_ROUTER = require('./adapters/badge/badgeRoute');
 const USER_ROUTER = require('./adapters/user/userRoute');
 const EXERCISE_SHEET_ROUTER = require('./adapters/exerciseSheet/exerciseSheetRouter');
+const DAY_SCORE_ROUTER = require('./adapters/dayScore/dayScoreRouter');
 
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -16,6 +17,7 @@ server.use(cookieParser());
 server.use('/api/badges', BADGE_ROUTER);
 server.use('/api/users', USER_ROUTER);
 server.use('/api/exercisesSheet', EXERCISE_SHEET_ROUTER);
+server.use('/api/dayScores', DAY_SCORE_ROUTER);
 
 server.listen(process.env.EXPRESS_PORT, () => {
     console.log(`Express initialized at ${process.env.EXPRESS_PORT}`);
