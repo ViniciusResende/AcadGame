2c0ad404eaad316cf48a63a4d71b602cf378c694
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-29 14:11:18 -0300

    feat: created new ExerciseToAdd interface to ExercisesSheet pages

---
+++ ui/web/src/data/interfaces/ExercisesSheetInterfaces.ts
@@ -0,0 +1,13 @@
+/**
+ * @category Interface
+ * @module ExercisesSheetInterfaces
+ */
+
+/**
+ * Exercise to add info data
+ */
+export interface IExerciseToAddInfoData {
+  exerciseId: number;
+  name: string;
+  type: string;
+}
