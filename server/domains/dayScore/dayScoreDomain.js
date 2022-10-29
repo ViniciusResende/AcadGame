3cf19f7fedad1ad603373eaf92881a74fc507240
Carlos Leão <caikleao@gmail.com>
2022-10-29 20:24:31 -0300

    Feat: Adding get user day scores function.

--- server/domains/dayScore/dayScoreDomain.js
+++ server/domains/dayScore/dayScoreDomain.js
@@ -1,12 +1,16 @@
-const QueryDayScore = require('../../gates/user/userExitGate');
+const QueryDayScore = require('../../gates/dayScore/dayScoreExitGate');
 
 class DayScore {
     async getUserDayScores(userId) {
         try {
-            
+            const QUERIED_DAY_SCORES = await QueryDayScore.getUserDayScores(userId);
+
+            return QUERIED_DAY_SCORES;
         }
         catch (err) {
             throw err;
         }
     }
 }
+
+module.exports = new DayScore;
