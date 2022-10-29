270564dfbe86456bfded61bc1f7be2b99f2573f3
Carlos Leão <caikleao@gmail.com>
2022-10-29 20:24:01 -0300

    Feat: Adding function to get user day scores.

--- server/gates/dayScore/dayScoreExitGate.js
+++ server/gates/dayScore/dayScoreExitGate.js
@@ -3,23 +3,15 @@ const DayScoreDBAdapter = require('../../adapters/dayScore/dayScoreDBAdapter');
 class QueryDayScore {
     async getUserDayScores(userId) {
         try {
-            const QUERIED_DAY_SCORES = await DayScoreDBAdapter.getUserDayScores(userId);
+            const QUERIED_DAY_SCORES = await DayScoreDBAdapter.findUserDayScores(userId);
 
+            let returnValues = [];
+
+            QUERIED_DAY_SCORES.forEach(queriedDayScore => {
+                returnValues.push(queriedDayScore).dataValues;
+            });
             
-
-            const allUserDayScores = QUERIED_DAY_SCORES.reduce((acc, item) => {
-                const currentYear = item.dataValues.year;
-                const currentWeek = item.dataValues.week;
-
-                if(acc[currentYear][currentWeek])
-                    acc[currentYear][currentWeek].push(item);
-                else if(acc[currentYear])
-                    Object.assign(acc, {...acc, [currentYear]: [item]})
-
-                if(acc[current])
-            })
-
-            return QUERIED_DAY_SCORES;
+            return returnValues;
         }
         catch(err) {
             throw err;
