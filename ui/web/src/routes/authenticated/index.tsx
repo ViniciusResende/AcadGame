61d3dec05322dc17a713c317d8ecb357046a3431
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-29 14:10:17 -0300

    feat: added new exercisesSheet authenticated routes

--- ui/web/src/routes/authenticated/index.tsx
+++ ui/web/src/routes/authenticated/index.tsx
@@ -5,11 +5,17 @@ import { Route } from 'react-router-dom';
 import AuthenticatedLayout from '../../components/Layout/Authenticated';
 
 /** Route elements */
+import RouteExercisesAdd from './RouteExercisesAdd';
+import RouteExercisesSheet from './RouteExercisesSheet';
 import RouteProfile from './RouteProfile';
 
 /** Exports */
 export default (
   <Route path="/" element={<AuthenticatedLayout />}>
     <Route path="/profile" element={<RouteProfile />} />
+    <Route path="/exercisesSheets">
+      <Route index element={<RouteExercisesSheet />} />
+      <Route path=":sheetId/add" element={<RouteExercisesAdd />} />
+    </Route>
   </Route>
 );
