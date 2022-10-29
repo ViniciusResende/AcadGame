65694404ef23007eb89f08c5ccb5c8814288d319
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-29 11:57:39 -0300

    feat: implementing ExercisesSheetAccess with default strategy

---
+++ shared/lib/src/access/exercise-sheet/ExercisesSheetAccess.ts
@@ -0,0 +1,40 @@
+/**
+ * @category Access
+ * @module ExercisesSheetAccess
+ */
+
+/** Classes */
+import { ExercisesSheetAccessStrategy } from './strategies/ExercisesSheetAccessStrategy';
+
+/** Interfaces */
+import { IApiAcadExercisesSheetGetAvailableToAddResponse } from '../../resource/api/acad/ApiAcadInterfaces';
+
+/**
+ * Class to provide access to exchanges' data.
+ */
+export class ExercisesSheetAccess {
+  #accessStrategy: ExercisesSheetAccessStrategy;
+
+  constructor() {
+    this.#accessStrategy = new ExercisesSheetAccessStrategy();
+  }
+
+  /**
+   * Retrieves an payload containing all available exercises to be add to a given
+   * exercise sheet and it's Id.
+   *
+   * @param authToken - An JWT like token that will be used to authenticate while
+   * fetching user data
+   * @param sheetId - The sheet id that intends to fetch available exercises to add
+   * @returns The response payload containing the fetched data
+   */
+  async getAvailableExercisesToAdd(
+    authToken: string,
+    sheetId: string
+  ): Promise<IApiAcadExercisesSheetGetAvailableToAddResponse> {
+    const exerciseSheetGetAvailableExercisesToAddResponseBody =
+      await this.#accessStrategy.getAvailableExercisesToAdd(authToken, sheetId);
+
+    return exerciseSheetGetAvailableExercisesToAddResponseBody;
+  }
+}
