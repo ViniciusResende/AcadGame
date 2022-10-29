3ea47f9d77f2736c21764641c4be20e9f49e91b2
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-29 14:09:44 -0300

    feat: adding new add square and dumbbell icons

--- ui/web/src/assets/svg/icons/index.ts
+++ ui/web/src/assets/svg/icons/index.ts
@@ -1,5 +1,7 @@
+import AddSquareIcon from './add-square.svg';
 import BullseyeArrowIcon from './bullseye-arrow.svg';
 import CheckIcon from './check.svg';
+import DumbbellIcon from './dumbbell.svg';
 import EyeIcon from './eye.svg';
 import EyeSlashIcon from './eye-slash.svg';
 import LogInIcon from './log-in.svg';
@@ -10,8 +12,10 @@ import TrophyStarIcon from './trophy-star.svg';
 import XMarkIcon from './xmark.svg';
 
 export {
+  AddSquareIcon,
   BullseyeArrowIcon,
   CheckIcon,
+  DumbbellIcon,
   EyeIcon,
   EyeSlashIcon,
   LogInIcon,
