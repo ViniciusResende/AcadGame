6b71b956c7751dd86b0ae40b163262621f9fd829
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-29 12:01:31 -0300

    feat: implementing ExercisesSheetManager to the Lib

---
+++ shared/lib/src/manager/exercise-sheet/ExercisesSheetManager.ts
@@ -0,0 +1,40 @@
+/**
+ * @category Manager
+ * @module ExercisesSheetManager
+ */
+
+/** Interfaces */
+import { IExerciseToAddInfo } from '../../data/interfaces/ExercisesSheetInterfaces';
+import { IApiAcadExercisesSheetGetAvailableToAddResponse } from '../../resource/api/acad/ApiAcadInterfaces';
+
+/** Utilities */
+import { Utilities } from '../../utils/Utilities';
+
+/** Engines */
+import { ExercisesSheetEngine } from '../../engine/exercises-sheet/ExercisesSheetEngine';
+
+/**
+ * Class to handle business logic related to the user data fetching at any level
+ * at the application
+ */
+export class ExercisesSheetManager extends Utilities.pubSub {
+  #exercisesSheetEngine: ExercisesSheetEngine;
+
+  constructor() {
+    super();
+    this.#exercisesSheetEngine = new ExercisesSheetEngine();
+  }
+
+  async getAllExercisesAvailableForSheet(
+    sheetId: string
+  ): Promise<IExerciseToAddInfo[] | null> {
+    const exercisesSheetGetAvailableForSheetResponse =
+      (await this.#exercisesSheetEngine.getAvailableExercisesToAdd(
+        sheetId
+      )) as IApiAcadExercisesSheetGetAvailableToAddResponse;
+
+    const { availableExercises } = exercisesSheetGetAvailableForSheetResponse;
+
+    return availableExercises || null;
+  }
+}
