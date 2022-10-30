ee2bde93950d2cccfbac364ae24a927a5d0f97a2
Carlos Leão <caikleao@gmail.com>
2022-10-29 22:53:36 -0300

    Feat: Adding /api/dayScores/user/last7days route to get all thecurrent week scores.

--- server/adapters/dayScore/dayScoreRouter.js
+++ server/adapters/dayScore/dayScoreRouter.js
@@ -15,4 +15,17 @@ Router.get('/', async (req, res) => {
     }
 });
 
+Router.get('/user/last7days', async (req, res) => {
+    try {
+        let userId = req.query.userId;
+
+        const LAST_7_SAY_SCORES = await DayScore.getLast7DaysScores(userId);
+
+        res.status(200).json(LAST_7_SAY_SCORES);
+    }
+    catch(err) {
+        res.status(500).send(err.message);
+    }
+})
+
 module.exports = Router;
