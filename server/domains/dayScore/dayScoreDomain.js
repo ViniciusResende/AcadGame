687ec4c7235a7ed68a899aeb979cc02653fa1951
Carlos Leão <caikleao@gmail.com>
2022-10-29 22:52:29 -0300

    Feat: Adding function to get the date of today and one week ago.

--- server/domains/dayScore/dayScoreDomain.js
+++ server/domains/dayScore/dayScoreDomain.js
@@ -1,3 +1,4 @@
+const { addDays } = require('date-fns');
 const QueryDayScore = require('../../gates/dayScore/dayScoreExitGate');
 
 class DayScore {
@@ -8,6 +9,23 @@ class DayScore {
             return QUERIED_DAY_SCORES;
         }
         catch (err) {
+            throw err;
+        }
+    }
+
+    async getLast7DaysScores(userId) {
+        try {
+            let today = new Date();
+            today.setHours(23);
+            today.setMinutes(59);
+            today.setSeconds(59);
+            const ONE_WEEK_AGO = addDays(today, -7);
+
+            const LAST_7_DAY_SCORES = QueryDayScore.getDaysScoresInInterval(userId, ONE_WEEK_AGO, today);
+
+            return LAST_7_DAY_SCORES;
+        }
+        catch(err) {
             throw err;
         }
     }
