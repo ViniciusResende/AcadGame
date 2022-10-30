75baae25e3d36af107bcdd0867b2db4a4f086e89
Carlos Leão <caikleao@gmail.com>
2022-10-29 22:49:46 -0300

    Feat: Adding function to get an array of 8 daily scores in a date interval.

--- server/gates/dayScore/dayScoreExitGate.js
+++ server/gates/dayScore/dayScoreExitGate.js
@@ -17,6 +17,30 @@ class QueryDayScore {
             throw err;
         }
     }
+
+    async getDaysScoresInInterval(userId, startDate, endDate) {
+        try {
+            const DAY_SCORES = await DayScoreDBAdapter.findUserDayScoresInInterval(userId, startDate, endDate);
+
+            let dayScores = [];
+            for (let index = 0; index < 8; index++) {
+                dayScores[index] = 0;
+            }
+
+            DAY_SCORES.forEach(dayScore => {
+                const dayScoreDate = dayScore.dataValues.date.getDate();
+                const dayScoreIndex = dayScoreDate - startDate.getDate();
+
+                
+                dayScores[dayScoreIndex] = dayScore.dataValues.score;
+            });
+
+            return dayScores;
+        }
+        catch(err) {
+            throw err;
+        }
+    }
 }
 
 module.exports = new QueryDayScore;
