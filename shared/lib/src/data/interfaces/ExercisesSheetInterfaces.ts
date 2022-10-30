a459791dc75c98776e337fc611080bd8b2c7f543
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-30 18:00:22 -0300

    feat: added new interface for the sheet exercises

--- shared/lib/src/data/interfaces/ExercisesSheetInterfaces.ts
+++ shared/lib/src/data/interfaces/ExercisesSheetInterfaces.ts
@@ -11,3 +11,16 @@ export interface IExerciseToAddInfo {
   name: string;
   type: string; //TODO add to exercises type enum
 }
+
+/**
+ * Sheet Exercise Info body interface.
+ */
+export interface ISheetExerciseInfo {
+  exerciseId: number;
+  name: string;
+  load: number | null;
+  time: number | null;
+  numRepetitions: number | null;
+  numSets: number;
+  isLoad: boolean;
+}
