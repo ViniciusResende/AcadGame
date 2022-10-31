299100cb947544afb2f72d83e5c95626f2d2737a
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-31 01:51:45 -0300

    Feat: Verifying if the user is already logged in.

--- server/adapters/authentication/authRoute.js
+++ server/adapters/authentication/authRoute.js
@@ -4,16 +4,23 @@ const AUTH_DOMAIN = require('../../domains/authentication/authDomain');
 
 // const AUTH = require('../../domains/authentication/authDomain');
 
-ROUTER.post('/login', async (req, res, next) => await AUTH_DOMAIN.login(req, res, next), async (req, res) => {
-    try {
-        res.status(200).send({
-            token: req.cookies['connect.sid'],
-            message: 'Enjoy your token! :)'
-        });
-    }
-    catch (err) {
-        res.status(500).send(err.message);
-    }
-});
+ROUTER.post('/login', 
+    async (req, res, next) => {
+        if (!req.isAuthenticated())
+            await AUTH_DOMAIN.login(req, res, next);
+        else
+            res.status(400).send('Você já está logado!');
+    },
+    async (req, res) => {
+        try {
+            res.status(200).send({
+                token: req.cookies['connect.sid'],
+                message: 'Enjoy your token! :)'
+            });
+        }
+        catch (err) {
+            res.status(500).send(err.message);
+        }
+    });
 
 module.exports = ROUTER;
