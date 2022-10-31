f6abddaa4f3a863da07b50fd83b583f98be17799
Pe-Guedss <pedro.og2002@gmail.com>
2022-10-31 02:55:57 -0300

    Feat: Creating error middlewares.

--- server/server.js
+++ server/server.js
@@ -25,15 +25,22 @@ const AUTH_ROUTER = require('./adapters/authentication/authRoute');
 const BODY_PARSER = require("body-parser");
 const COOKIE_PARSER = require("cookie-parser");
 
+
+const errorHandler = (err, req, res, next) => {
+    console.log('fbadsjkfbadsfbadskb');
+    res.status(500).send(err.message);
+}
+
+
 SERVER.use(EXPRESS.json());
 SERVER.use(BODY_PARSER.urlencoded({extended:true}));
 SERVER.use(BODY_PARSER.json());
 SERVER.use(COOKIE_PARSER());
 
-SERVER.use('/badges', BADGE_ROUTER);
-SERVER.use('/exercises', EXERCISE_ROUTER);
-SERVER.use('/users', USER_ROUTER);
-SERVER.use('/auth', AUTH_ROUTER);
+SERVER.use('/badges', BADGE_ROUTER, errorHandler);
+SERVER.use('/exercises', EXERCISE_ROUTER, errorHandler);
+SERVER.use('/users', USER_ROUTER, errorHandler);
+SERVER.use('/auth', AUTH_ROUTER, errorHandler);
 
 SERVER.listen(process.env.EXPRESS_PORT, () => {
     console.log(`Express initialized at ${process.env.EXPRESS_PORT}`);
