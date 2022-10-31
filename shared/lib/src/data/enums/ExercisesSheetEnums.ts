23d4d6c6902adf7604830472c29382df14621ebc
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-10-31 12:39:09 -0300

    feat: adding new exercise type enum

---
+++ shared/lib/src/data/enums/ExercisesSheetEnums.ts
@@ -0,0 +1,19 @@
+/**
+ * @category Enum
+ * @module ExercisesSheetEnums
+ */
+
+/**
+ * Enumerated values for Exercises Sheets exercise type.
+ */
+export enum ExercisesSheetExerciseToAddTypeEnum {
+  BICEPS = 'biceps',
+  CALF = 'calf',
+  CHEST = 'chest',
+  GLUTEAL = 'gluteal',
+  LATS = 'lats',
+  POSTERIOR_THIGH = 'posterior_thigh',
+  QUADRICEPS = 'quadriceps',
+  SHOULDER = 'shoulder',
+  TRICEPS = 'triceps',
+}
