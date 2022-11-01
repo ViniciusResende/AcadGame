f898c2c7818800ddb96541ee91e68760b4141c2c
Carlos Leão <caikleao@gmail.com>
2022-11-01 09:49:22 -0300

    Feat: Creating /api/dailyScores/user/add route to create new daily scores.

--- server/adapters/dayScore/dayScoreRouter.js
+++ server/adapters/dayScore/dayScoreRouter.js
@@ -19,13 +19,27 @@ Router.get('/user/last7days', async (req, res) => {
     try {
         let userId = req.query.userId;
 
-        const LAST_7_SAY_SCORES = await DayScore.getLast7DaysScores(userId);
+        const LAST_7_DAYS_SCORES = await DayScore.getLast7DaysScores(userId);
 
-        res.status(200).json(LAST_7_SAY_SCORES);
+        res.status(200).json(LAST_7_DAYS_SCORES);
     }
     catch(err) {
         res.status(500).send(err.message);
     }
-})
+});
+
+Router.post('/user/add', async (req, res) => {
+    try {
+        const USER_ID = req.query.userId;
+        const SENT_EXERCISES_INFO = req.body;
+
+        await DayScore.createDailyScore(USER_ID, SENT_EXERCISES_INFO);
+
+        res.status(201).json('Pontuações diárias cadastradas com sucesso!');
+    }
+    catch(err) {
+        res.status(500).send(err.message);
+    }
+});
 
 module.exports = Router;
