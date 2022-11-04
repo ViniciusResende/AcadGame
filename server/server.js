04b77c318af5203ed0f45cf7b16d76c4a10795bd
Carlos Leão <caikleao@gmail.com>
2022-11-03 23:54:59 -0300

    Feat: Adding /api/auth route to server.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const server = Express();
 
 const BADGE_ROUTER = require('./adapters/badge/badgeRoute');
 const USER_ROUTER = require('./adapters/user/userRoute');
+const AUTH_ROUTER = require('./adapters/authentication/authRoute');
 const EXERCISE_SHEET_ROUTER = require('./adapters/exerciseSheet/exerciseSheetRouter');
 const DAY_SCORE_ROUTER = require('./adapters/dayScore/dayScoreRouter');
 
@@ -18,6 +19,7 @@ server.use('/api/badges', BADGE_ROUTER);
 server.use('/api/users', USER_ROUTER);
 server.use('/api/exercisesSheet', EXERCISE_SHEET_ROUTER);
 server.use('/api/dailyScores', DAY_SCORE_ROUTER);
+server.use('/api/auth', AUTH_ROUTER);
 
 server.listen(process.env.EXPRESS_PORT, () => {
     console.log(`Express initialized at ${process.env.EXPRESS_PORT}`);
