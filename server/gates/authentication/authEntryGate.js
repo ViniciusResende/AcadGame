0639d5af685308f8733249302c86cf1174cd41b8
Carlos Leão <caikleao@gmail.com>
2022-11-03 23:54:08 -0300

    Feat: Creating authentication entry gate. Contains registerUser and authenticateUser functions, which generate and return a json web token.

---
+++ server/gates/authentication/authEntryGate.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../../config/auth.json');
+
+const userDomain = require('../../domains/user/userDomain');
+
+function generateToken(params = {}) {
+   return jwt.sign(params, authConfig.secret, {
+       expiresIn: 86400, // token expires in one day
+   });
+}
+
+class AuthenticationEntryGate {
+   async registerUser(userInfo) {
+      try {
+         const user = await userDomain.createUser(userInfo);
+
+         const token = generateToken({ id: user.id });
+
+         return { token };
+      }
+      catch(err) {
+         throw err;
+      }
+   }
+
+   async authenticateUser(email, password) {
+      try {
+         const USER = await userDomain.getUserByEmail(email);
+
+         if(!await bcrypt.compare(password, USER.password))
+            throw new Error('Senha inválida.');
+
+         const token = generateToken({ id: USER.id });
+
+         return { token };
+      }
+      catch(err) {
+         throw err;
+      }
+   }
+}
+
+module.exports = new AuthenticationEntryGate;
