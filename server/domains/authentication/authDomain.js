4cb00dfd3686a4fe13fc816411dd8d7350c7def9
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-04 23:11:34 -0300

    Feat: Creating an authentication domain.

---
+++ server/domains/authentication/authDomain.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const userDomain = require('../../domains/user/userDomain');
+
+function generateToken(params = {}) {
+   return jwt.sign(params, process.env.JWT_SECRET, {
+       expiresIn: process.env.COOKIE_DURATION,
+   });
+}
+
+class authDomain {
+   async registerUser(userInfo) {
+      try {
+         const user = await userDomain.createUser(userInfo);
+
+         const token = generateToken({ id: user.id });
+
+         return { token };
+      }
+      catch(err) {
+         throw err;
+      }
+   }
+
+   async authenticateUser(email, password) {
+      try {
+         const USER = await userDomain.getUserByEmail(email);
+
+         if(!await bcrypt.compare(password, USER.password))
+            throw new Error('Senha inválida.');
+
+         const token = generateToken({ id: USER.id });
+
+         return { token };
+      }
+      catch(err) {
+         throw err;
+      }
+   }
+}
+
+module.exports = new authDomain;
