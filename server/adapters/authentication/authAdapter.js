b0f8deacbb958107b54c41db8a140d019c156ce9
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-04 23:19:42 -0300

    Feat: Creating an authentication adapter.

---
+++ server/adapters/authentication/authAdapter.js
@@ -0,0 +1,23 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+class authAdapter {
+    async generateToken(params = {}) {
+        const TOKEN = jwt.sign(params, process.env.JWT_SECRET, {
+            expiresIn: process.env.COOKIE_DURATION,
+        });
+
+        return TOKEN;
+    }
+
+    async checkPasswords(inputedPassword, hashedPassword) {
+        const MATCH = await bcrypt.compare(inputedPassword, hashedPassword);
+
+        if(!MATCH)
+            return false;
+        else
+            return true;
+    }
+}
+
+module.exports = new authAdapter;
