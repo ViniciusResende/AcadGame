d00ed2848b2f345a35ffee08b6681868a13b855f
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-11-06 11:31:58 -0300

    feat: added new svg icons

--- ui/web/src/assets/svg/icons/index.ts
+++ ui/web/src/assets/svg/icons/index.ts
@@ -5,14 +5,26 @@ import CalendarIcon from './calendar.svg';
 import CheckIcon from './check.svg';
 import ChevronIcon from './chevron.svg';
 import ChevronLightIcon from './chevron-light.svg';
+import CrowdTableIcon from './crowd-table.svg';
 import DumbbellIcon from './dumbbell.svg';
 import EyeIcon from './eye.svg';
 import EyeSlashIcon from './eye-slash.svg';
 import FlagIcon from './flag.svg';
+import GroupPictureIcon from './group-picture.svg';
+import IdentifyCardIcon from './identify-card.svg';
+import LaptopIcon from './laptop.svg';
 import LogInIcon from './log-in.svg';
 import MetricsChartIcon from './metrics-chart.svg';
+import MoneyImprovementIcon from './money-improvement.svg';
 import PencilIcon from './pencil.svg';
+import PeopleConnectionIcon from './people-connection.svg';
+import PeopleCrowdIcon from './people-crowd.svg';
 import PersonIcon from './person.svg';
+import PersonProfileIcon from './person-profile.svg';
+import PersonRunIcon from './person-run.svg';
+import PresentationIcon from './presentation.svg';
+import QuestionPersonIcon from './question-person.svg';
+import StarPersonIcon from './star-person.svg';
 import ToDoListIcon from './todo-list.svg';
 import TrophyStarIcon from './trophy-star.svg';
 import XMarkIcon from './xmark.svg';
@@ -25,14 +37,26 @@ export {
   CheckIcon,
   ChevronIcon,
   ChevronLightIcon,
+  CrowdTableIcon,
   DumbbellIcon,
   EyeIcon,
   EyeSlashIcon,
   FlagIcon,
+  GroupPictureIcon,
+  IdentifyCardIcon,
+  LaptopIcon,
   LogInIcon,
   MetricsChartIcon,
+  MoneyImprovementIcon,
   PencilIcon,
+  PeopleConnectionIcon,
+  PeopleCrowdIcon,
   PersonIcon,
+  PersonProfileIcon,
+  PersonRunIcon,
+  PresentationIcon,
+  QuestionPersonIcon,
+  StarPersonIcon,
   ToDoListIcon,
   TrophyStarIcon,
   XMarkIcon,
