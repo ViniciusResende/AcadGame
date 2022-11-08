1a98cc0023801a291ea431d25be503036a4b06fd
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-08 00:19:45 -0300

    Fix: Removing merge conflicts.

--- server/server.js
+++ server/server.js
@@ -11,8 +11,6 @@ const DAY_SCORE_ROUTER = require('./adapters/dayScore/dayScoreRouter');
 const AUTHENTICATION = require('./utils/authMiddleware');
 const ERROR_HANDLING = require('./utils/errorMiddleware');
 
-const IS_AUTHENTICATED = require('./domains/authentication/authDomain').isLoggedIn;
-
 server.use(cors());
 server.use(Express.json());
 server.use(bodyParser.urlencoded({extended:true}));
@@ -24,21 +22,6 @@ server.use('/api/users', AUTHENTICATION, USER_ROUTER, ERROR_HANDLING);
 server.use('/api/exercisesSheet', AUTHENTICATION, EXERCISE_SHEET_ROUTER, ERROR_HANDLING);
 server.use('/api/dailyScores', AUTHENTICATION, DAY_SCORE_ROUTER, ERROR_HANDLING);
 server.use('/api/auth', AUTH_ROUTER, ERROR_HANDLING);
-
-const errorHandler = (err, req, res, next) => {
-    res.status(500).send(err.message);
-}
-
-
-SERVER.use(EXPRESS.json());
-SERVER.use(BODY_PARSER.urlencoded({extended:true}));
-SERVER.use(BODY_PARSER.json());
-SERVER.use(COOKIE_PARSER());
-
-SERVER.use('/badges', IS_AUTHENTICATED, BADGE_ROUTER, errorHandler);
-SERVER.use('/exercises', IS_AUTHENTICATED, EXERCISE_ROUTER, errorHandler);
-SERVER.use('/users', USER_ROUTER, errorHandler);
-SERVER.use('/auth', AUTH_ROUTER, errorHandler);
 
 SERVER.listen(process.env.EXPRESS_PORT, () => {
     console.log(`Express initialized at ${process.env.EXPRESS_PORT}`);
