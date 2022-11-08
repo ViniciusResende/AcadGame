1c39cdbf7463e4aa0ce82887703eeedf9a6ccb47
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-08 17:55:26 -0300

    Feat: Defining a route to get every pending friendship request.

--- server/adapters/gymPal/gymPalRouter.js
+++ server/adapters/gymPal/gymPalRouter.js
@@ -2,7 +2,16 @@ const ROUTER = require('express').Router();
 
 ROUTER.get('/me/accepted', async (req, res, next) => {
     try {
-        res.status(200).send('Essa rota é responsável por retornar todos os amigos do usuário que realizou a requisição.')
+        res.status(200).send('Essa rota é responsável por retornar todos os amigos do usuário que realizou a requisição.');
+    }
+    catch (err) {
+        next(err);
+    }
+});
+
+ROUTER.get('/me/pending', async (req, res, next) => {
+    try {
+        res.status(200).send('Essa rota retorna todos os pedido de amizade pendentes do usuário que realizou a requisição.');
     }
     catch (err) {
         next(err);
