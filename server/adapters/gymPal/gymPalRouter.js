dba5e61104e16fb7845e501f62bea7237c2b95af
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-08 17:57:10 -0300

    Ref: Removing the "/me" identifier in the routes paths.

--- server/adapters/gymPal/gymPalRouter.js
+++ server/adapters/gymPal/gymPalRouter.js
@@ -1,6 +1,6 @@
 const ROUTER = require('express').Router();
 
-ROUTER.get('/me/accepted', async (req, res, next) => {
+ROUTER.get('/accepted', async (req, res, next) => {
     try {
         res.status(200).send('Essa rota é responsável por retornar todos os amigos do usuário que realizou a requisição.');
     }
@@ -9,7 +9,7 @@ ROUTER.get('/me/accepted', async (req, res, next) => {
     }
 });
 
-ROUTER.get('/me/pending', async (req, res, next) => {
+ROUTER.get('/pending', async (req, res, next) => {
     try {
         res.status(200).send('Essa rota retorna todos os pedido de amizade pendentes do usuário que realizou a requisição.');
     }
