4dac8652cd7308c54a997e790976e2c103f450bb
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-11-08 06:41:20 -0300

    feat: added fetch to user ranking on RouteRanking

--- ui/web/src/routes/authenticated/RouteRanking.tsx
+++ ui/web/src/routes/authenticated/RouteRanking.tsx
@@ -5,32 +5,60 @@ import React, { useEffect, useState } from 'react';
 import Ranking from '../../components/Authenticated/Ranking';
 
 /** Library */
-import Lib from 'acad-game-lib';
+import Lib, { UserProfilePictureEnum } from 'acad-game-lib';
 
 /** Interfaces */
-import { IWeekRankingInfo } from '../../data/interfaces/RankingInterfaces';
+import {
+  IUserRankingInfo,
+  IWeekRankingInfo,
+} from '../../data/interfaces/RankingInterfaces';
 
 /** Constants */
+const DEFAULT_USER_RANKING = {
+  firstPlaceRankUser: {
+    userId: '0',
+    nickname: 'Apelido',
+    score: 0,
+    profileIcon: UserProfilePictureEnum.DEFAULT,
+  },
+  averageScore: 0,
+  userRankInfo: {
+    userId: '0',
+    nickname: 'Apelido',
+    score: 0,
+    profileIcon: UserProfilePictureEnum.DEFAULT,
+    userRank: 0,
+  },
+};
 const DEFAULT_WEEK_RANKING = {
   podiumUsers: [],
   nonPodiumUsers: [],
 };
 
 function RouteRanking() {
+  const [userRanking, setUserRanking] =
+    useState<IUserRankingInfo>(DEFAULT_USER_RANKING);
   const [weekRankings, setWeekRankings] =
     useState<IWeekRankingInfo>(DEFAULT_WEEK_RANKING);
 
   useEffect(() => {
+    async function getUserRanking() {
+      const getUserRankingResponse = await Lib.ranking.getUserRanking();
+
+      return getUserRankingResponse;
+    }
+
     async function getWeekRankings() {
       const getWeekRankingsResponse = await Lib.ranking.getWeekRankings();
 
       return getWeekRankingsResponse;
     }
 
+    getUserRanking().then((response) => response && setUserRanking(response));
     getWeekRankings().then((response) => response && setWeekRankings(response));
   }, []);
 
-  return <Ranking weekRankingInfo={weekRankings} />;
+  return <Ranking userRanking={userRanking} weekRankingInfo={weekRankings} />;
 }
 
 /** Exports */
