17f5a3d277625d4bf5414b9e0b58a1f5c365d80f
Vinicius Alves <viniciusfariaresende@gmail.com>
2022-11-08 07:16:12 -0300

    feat: implementing 'Seu Ranking' card to Ranking Page

--- ui/web/src/components/Authenticated/Ranking/Ranking.tsx
+++ ui/web/src/components/Authenticated/Ranking/Ranking.tsx
@@ -3,13 +3,18 @@ import React from 'react';
 
 /** React Components */
 import PodiumUser from './elements/PodiumUser';
+import ProfileUser from './elements/ProfileUser';
+import ProfileUserChart from './elements/ProfileUserChart';
 import WeekPodiumChart from './elements/WeekPodiumChart';
 
 /** React Template Components */
 import DefaultCardBox from '../../Template/DefaultCardBox';
 
 /** Interfaces */
-import { IWeekRankingInfo } from '../../../data/interfaces/RankingInterfaces';
+import {
+  IUserRankingInfo,
+  IWeekRankingInfo,
+} from '../../../data/interfaces/RankingInterfaces';
 
 /** Styles */
 import './Ranking.scss';
@@ -18,15 +23,22 @@ import './Ranking.scss';
 import { MetricsChartIcon, WifiSignalIcon } from '../../../assets/svg/icons';
 
 type RankingComponentProps = {
+  userRanking: IUserRankingInfo;
   weekRankingInfo: IWeekRankingInfo;
 };
-function RankingComponent({ weekRankingInfo }: RankingComponentProps) {
+function RankingComponent({
+  userRanking,
+  weekRankingInfo,
+}: RankingComponentProps) {
   return (
     <div className="ranking-page__container">
       <section className="ranking-page__upper-cards">
         <DefaultCardBox icon={<WifiSignalIcon />} title="Podium da Semana">
           <div className="ranking-page__podium-card">
-            <WeekPodiumChart podiumUsers={weekRankingInfo.podiumUsers} />
+            <div className="ranking-page__podium-chart">
+              <WeekPodiumChart podiumUsers={weekRankingInfo.podiumUsers} />
+              <span className="ranking-page__podium-line" />
+            </div>
             <div className="ranking-page__podium-users">
               {weekRankingInfo.podiumUsers.map((podiumUser, idx) => (
                 <PodiumUser podiumUserInfo={podiumUser} position={idx + 1} />
@@ -35,7 +47,10 @@ function RankingComponent({ weekRankingInfo }: RankingComponentProps) {
           </div>
         </DefaultCardBox>
         <DefaultCardBox icon={<MetricsChartIcon />} title="Seu Ranking">
-          <div className="ranking-page__rank-profile"></div>
+          <div className="ranking-page__rank-profile-card">
+            <ProfileUser profileUserInfo={userRanking.userRankInfo} />
+            <ProfileUserChart userRankingInfo={userRanking} />
+          </div>
         </DefaultCardBox>
       </section>
     </div>
