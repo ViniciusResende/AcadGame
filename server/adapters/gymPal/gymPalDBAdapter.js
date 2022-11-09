bc00d7598ee02821263829f20237354ecf2ffc89
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-09 01:23:28 -0300

    Feat: Removing unnecessary require.

--- server/adapters/gymPal/gymPalDBAdapter.js
+++ server/adapters/gymPal/gymPalDBAdapter.js
@@ -2,8 +2,6 @@ const GYM_PALS = require('../../infrastructure/models/gymPals');
 
 const { Op } = require('sequelize');
 
-const SERVER_ERROR = require('../../utils/serverErrors');
-
 class GymPalsDatabaseAdapter {
     async addNewPal(senderUserId, receiverUserId) {
         try {
