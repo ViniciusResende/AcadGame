26c262fd4ca4b85a0de672389d05dd379a41771a
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-09 00:41:00 -0300

    Feat: Defining a domain method to request friendships.

--- server/domains/gymPal/gymPalDomain.js
+++ server/domains/gymPal/gymPalDomain.js
@@ -1,9 +1,28 @@
 const SERVER_ERROR = require('../../utils/serverErrors');
 
+const USER_DOMAIN = require('../user/userDomain');
+
 const QUERY_GYM_PALS = require('../../gates/gymPal/gymPalExitGate');
 
 class GymPals {
+    async requestFriendship(senderUserId, receiverUserId) {
+        try {
+            const USER_RECEIVER_EXISTS = await USER_DOMAIN.getSingleUser(receiverUserId);
+            if (!USER_RECEIVER_EXISTS) {
+                const error = new SERVER_ERROR;
+                error.ServerError(400, 'Este usuário não existe.');
 
+                throw error;
+            }
+
+            const NEW_FRIENDSHIP = await QUERY_GYM_PALS.addPal(senderUserId, receiverUserId);
+
+            return NEW_FRIENDSHIP;
+        }
+        catch (err) {
+            throw err;
+        }
+    }
 }
 
 module.exports = new GymPals;
