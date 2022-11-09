2af8d00e3a535c1e4d4c677bd4e93bcacb05c400
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-09 00:20:13 -0300

    Feat: Defining the gym pals db adapter.

---
+++ server/adapters/gymPal/gymPalDBAdapter.js
@@ -0,0 +1,11 @@
+const GYM_PALS = require('../../infrastructure/models/gymPals');
+
+const { Op } = require('sequelize');
+
+const SERVER_ERROR = require('../../utils/serverErrors');
+
+class GymPalsDatabaseAdapter {
+
+}
+
+module.exports = new GymPalsDatabaseAdapter;
