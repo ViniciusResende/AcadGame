b944895df46adf00217c8b2e2d21819ad43a919a
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-09 03:16:26 -0300

    Feat: Requiring authentication to access the "/gymPals" route.

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ SERVER.use('/api/users', AUTHENTICATION, USER_ROUTER, ERROR_HANDLING);
 SERVER.use('/api/exercisesSheet', AUTHENTICATION, EXERCISE_SHEET_ROUTER, ERROR_HANDLING);
 SERVER.use('/api/dailyScores', AUTHENTICATION, DAY_SCORE_ROUTER, ERROR_HANDLING);
 SERVER.use('/api/auth', AUTH_ROUTER, ERROR_HANDLING);
-SERVER.use('/api/gymPals', GYM_PALS_ROUTER, ERROR_HANDLING);
+SERVER.use('/api/gymPals', AUTHENTICATION, GYM_PALS_ROUTER, ERROR_HANDLING);
 
 SERVER.listen(process.env.EXPRESS_PORT, () => {
     console.log(`Express initialized at ${process.env.EXPRESS_PORT}`);
