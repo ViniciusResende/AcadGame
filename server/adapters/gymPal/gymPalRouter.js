570b73b6abf59b39abaf3595ac831d8a68eb848e
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-09 00:11:56 -0300

    Ref: Renaming the "/accepted" route to "/connections" to better represent the request.

--- server/adapters/gymPal/gymPalRouter.js
+++ server/adapters/gymPal/gymPalRouter.js
@@ -1,6 +1,6 @@
 const ROUTER = require('express').Router();
 
-ROUTER.get('/accepted', async (req, res, next) => {
+ROUTER.get('/connections', async (req, res, next) => {
     try {
         res.status(200).send('Essa rota é responsável por retornar todos os amigos do usuário que realizou a requisição.');
     }
