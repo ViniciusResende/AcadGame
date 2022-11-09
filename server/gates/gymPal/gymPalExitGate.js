f7fc6e5eb00d869a3eeef752fa1c3ab543f9132d
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-09 01:06:36 -0300

    Ref: Changing the receiver user existence verifier to the exit gat.

--- server/gates/gymPal/gymPalExitGate.js
+++ server/gates/gymPal/gymPalExitGate.js
@@ -2,9 +2,19 @@ const SERVER_ERROR = require('../../utils/serverErrors');
 
 const GYM_PALS_DB_ADAPTER = require('../../adapters/gymPal/gymPalDBAdapter');
 
+const USER_DOMAIN = require('../../domains/user/userDomain');
+
 class QueryGymPals {
     async addPal(senderUserId, receiverUserId) {
         try {
+            const USER_RECEIVER_EXISTS = await USER_DOMAIN.getSingleUser(receiverUserId);
+            if (!USER_RECEIVER_EXISTS) {
+                const error = new SERVER_ERROR;
+                error.ServerError(400, 'Este usuário não existe.');
+
+                throw error;
+            }
+
             const NEW_FRIENDSHIP = await GYM_PALS_DB_ADAPTER.addNewPal(senderUserId, receiverUserId);
 
             if (!NEW_FRIENDSHIP) {
