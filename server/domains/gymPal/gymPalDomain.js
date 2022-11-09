e410e1a2d9315146164bca000e510c86dff2cc36
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-09 00:36:31 -0300

    Feat: Defining the Gym Pals domain file.

---
+++ server/domains/gymPal/gymPalDomain.js
@@ -0,0 +1,9 @@
+const SERVER_ERROR = require('../../utils/serverErrors');
+
+const QUERY_GYM_PALS = require('../../gates/gymPal/gymPalExitGate');
+
+class GymPals {
+
+}
+
+module.exports = new GymPals;
