6edf720c41c89a7a7ec5ff5befef30734712222b
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-09 00:30:25 -0300

    Feat: Defining a class to make queries to the database.

---
+++ server/gates/gymPal/gymPalExitGate.js
@@ -0,0 +1,9 @@
+const SERVER_ERROR = require('../../utils/serverErrors');
+
+const GYM_PALS_DB_ADAPTER = require('../../adapters/gymPal/gymPalDBAdapter');
+
+class QueryGymPals {
+    
+}
+
+module.exports = new QueryGymPals;
