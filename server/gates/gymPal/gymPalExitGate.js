df86f96cf2edb0720066110dc03ac12b92929800
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-09 00:34:42 -0300

    Feat: Defining a method to make a new friendship request.

--- server/gates/gymPal/gymPalExitGate.js
+++ server/gates/gymPal/gymPalExitGate.js
@@ -3,7 +3,23 @@ const SERVER_ERROR = require('../../utils/serverErrors');
 const GYM_PALS_DB_ADAPTER = require('../../adapters/gymPal/gymPalDBAdapter');
 
 class QueryGymPals {
-    
+    async addPal(senderUserId, receiverUserId) {
+        try {
+            const NEW_FRIENDSHIP = await GYM_PALS_DB_ADAPTER.addNewPal(senderUserId, receiverUserId);
+
+            if (!NEW_FRIENDSHIP) {
+                let error = new SERVER_ERROR;
+                error.ServerError(500, 'Não foi possível efetuar o pedido de amizade.');
+
+                throw error;
+            }
+
+            return NEW_FRIENDSHIP;
+        }
+        catch (err) {
+            throw err;
+        }
+    }
 }
 
 module.exports = new QueryGymPals;
