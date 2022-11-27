5e4f5570df9e6104daa180d68a08768dc1c64707
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-26 23:31:31 -0300

    Feat: Adding more tests to file.

--- server/adapters/user/tests/userPut.test.js
+++ server/adapters/user/tests/userPut.test.js
@@ -33,6 +33,45 @@ describe('User updation', () => {
         expect(CURR_USER.nickname).toBe('pedrinho_testes_ltda.');
     });
 
+    test('Should update user\'s e-mail (PUT /api/users/me)', async () => {
+        const PREV_USER = await USER_DOMAIN.getSingleUser(1);
+        expect(PREV_USER.email).toBe(USER_COLLECTION[0].email);
+
+        await USER_DOMAIN.updateUserInfo(1, {
+            email: 'pedro@pedro.pedro'
+        });
+
+        const CURR_USER = await USER_DOMAIN.getSingleUser(1);
+        expect(CURR_USER.email).toBe('pedro@pedro.pedro');
+    });
+
+    test('Should fail to update user\'s e-mail, since there is already another user registered with the provided one (PUT /api/users/me)', async () => {
+        try {    
+            await USER_DOMAIN.updateUserInfo(1, {
+                email: 'test@testt.com.test'
+            });
+
+            expect(false).toBeTruthy();
+        }
+        catch (err) {
+            expect(err.message).toBe("Este e-mail já é utilizado por outra conta.");
+        }
+    });
+
+    test('Should fail to update user, since the given property is not valid (PUT /api/users/me)', async () => {
+        try {    
+            await USER_DOMAIN.updateUserInfo(1, {
+                isAMcLovinLover: true
+            });
+
+            expect(false).toBeTruthy();
+        }
+        catch (err) {
+            expect(err.message).toBe("Uma das propriedades fornecidas não é válida.");
+        }
+    });
+    
+
     afterAll(async () => {
         await USER.sync({
             force: true
