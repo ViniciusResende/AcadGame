bad31277f991f7e50e0db61c0d9f8601cfacaa17
Pe-Guedss <pedro.og2002@gmail.com>
2022-11-27 21:02:09 -0300

    Feat: Developing tests to check getUserDayScores and getLast7DaysScores.

---
+++ server/adapters/dayScore/tests/scoreGetters.test.js
@@ -0,0 +1,72 @@
+jest.mock('../../../infrastructure/db');
+
+const DAILY_SCORE_DOMAIN = require('../../../domains/dayScore/dayScoreDomain');
+const USER_DOMAIN = require('../../../domains/user/userDomain');
+
+const DAILY_SCORE = require('../../../infrastructure/models/dayScore');
+const USER = require('../../../infrastructure/models/user');
+
+describe('User updation', () => {
+
+    const USER_COLLECTION = [{
+        nickname: "testUser",
+        email: "test@test.com.test",
+        password: "test123"
+    }, {
+        nickname: "testUser1",
+        email: "test1@test.com.test",
+        password: "test123"
+    }, {
+        nickname: "testUser2",
+        email: "test2@test.com.test",
+        password: "test123"
+    }, {
+        nickname: "testUser3",
+        email: "test3@test.com.test",
+        password: "test123"
+    }];
+
+    const EXERCISE_INFO = {
+        numRepetitions: 10,
+        numSets: 100,
+        isLoad: true
+    };
+
+    beforeAll(async () => {
+        let id = 1;
+        for (const USER_INFO of USER_COLLECTION) {
+            await USER_DOMAIN.createUser(USER_INFO);
+            for (let exerciseAmount = 0; exerciseAmount < id; exerciseAmount++) {
+                await DAILY_SCORE_DOMAIN.createDailyScore(id, [EXERCISE_INFO]);
+            }
+            id += 1;
+        }
+    });
+
+    test('Should get all user\'s exercises of the day (GET /api/dailyScores)', async () => {
+        const CURR_SCORE = await DAILY_SCORE_DOMAIN.getUserDayScores(4);
+
+        expect(CURR_SCORE.length).toBeGreaterThanOrEqual(4);
+    });
+
+    test('Should get every exercise the user made in the week (GET /api/dailyScores)', async () =>{
+        const USER_WEEK_EXERCISES = await DAILY_SCORE_DOMAIN.getLast7DaysScores(3);
+
+        // This is wrong and should be changed. We could start by changing the date column data type to DATEONLY, instead of DATE.
+        expect(USER_WEEK_EXERCISES.length).toBe(8);
+        for (let weekDay = 0; weekDay < USER_WEEK_EXERCISES.length - 1; weekDay++) {
+            expect(USER_WEEK_EXERCISES[weekDay].score).toBe(0);
+        }
+        expect(USER_WEEK_EXERCISES[7].score).toBe(3000);
+    });
+
+    afterAll(async () => {
+        await USER.sync({
+            force: true
+        });
+
+        await DAILY_SCORE.sync({
+            force: true
+        });
+   });
+});
